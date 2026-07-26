b113dd7af223896c91040771e7cac097b89cadd3
Aryan1736 <aryanbagchi17@gmail.com>
2026-07-26 21:23:29 +0530

    first commit

---
+++ src/main/java/com/aryan/fulfillx/FulfillxApplication.java
@@ -0,0 +1,13 @@
+package com.aryan.fulfillx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulfillxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulfillxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aryan/fulfillx/FulfillxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryan.fulfillx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulfillxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
